1ceb6f51f5a211391c1afee62e2e04d1b4fefb5c
AKHILA-SW10 <jayalaxmisweety10@gmail.com>
2023-01-29 12:55:42 -0600

    Added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { WarningComponent } from 'src/WarningAlert/warning.component';
 
 import { AppComponent } from './app.component';
+import { SuccessComponent } from 'src/SuccessAlert/success.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SuccessComponent,
+    WarningComponent,
   ],
   imports: [
     BrowserModule
